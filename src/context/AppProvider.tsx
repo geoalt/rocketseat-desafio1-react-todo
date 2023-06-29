8daef620deefaa61e0b4380b7dc64467dffb2c98
Diogo Martins <me.martinsfe@gmail.com>
2023-06-29 14:45:46 -0300

    feat: create context

---
+++ src/context/AppProvider.tsx
@@ -0,0 +1,28 @@
+import React, { ReactNode, createContext, useMemo, useState } from 'react';
+
+interface ITask {
+  id: string;
+  content: string;
+  isComplete: boolean;
+}
+
+interface IContext {
+  task: ITask[];
+  setTask: React.Dispatch<React.SetStateAction<ITask[]>>;
+}
+
+export const AppContext = createContext<IContext>({} as IContext);
+
+export function AppProvider({ children }: { children: ReactNode }) {
+  const [task, setTask] = useState<ITask[]>([]);
+
+  const values = useMemo(
+    () => ({
+      task,
+      setTask,
+    }),
+    [task]
+  );
+
+  return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './scss/style.scss'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { AppProvider } from './context/AppProvider.tsx';
+import './scss/style.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </React.StrictMode>
+);
